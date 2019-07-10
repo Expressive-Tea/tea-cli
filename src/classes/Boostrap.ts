182675095f770e250c87ff489314f9b2baf6b260
Diego Resendez <diego.resendez@zero-oneit.com>
2019-07-10 18:33:08 -0500

    [CREATE] Added create command.

--- src/classes/Boostrap.ts
+++ src/classes/Boostrap.ts
@@ -3,23 +3,34 @@ import * as path from 'path';
 import MetaData from '../classes/MetaData';
 import { COMMANDS_KEY } from '../constants/constants';
 
+interface BootstrapOptions {
+  root: string;
+}
+
+const DEFAULT = {
+  root: path.resolve(__dirname, '../../../')
+};
+
 export default class Bootstrap {
   static getInstance() {
-    return Bootstrap.instance ? Bootstrap.instance : new Bootstrap();
+    return Bootstrap.instance ? Bootstrap.instance : new Bootstrap(DEFAULT);
   }
 
   private static instance: Bootstrap;
   private $pkg = require(path.resolve(__dirname, '../../../package.json'));
+  private options: BootstrapOptions;
 
-  constructor() {
+  constructor(options: BootstrapOptions = DEFAULT) {
     if (Bootstrap.instance) {
       return Bootstrap.instance;
     }
 
     const commands = MetaData.get(COMMANDS_KEY, this);
+
+    this.options = options;
     $prog.version(this.$pkg.version);
 
-    commands.forEach(c => c($prog));
+    commands.forEach(c => c($prog, options));
     return this;
   }
 
--- src/commands/create/index.ts
+++ src/commands/create/index.ts
@@ -1,12 +1,58 @@
-module.exports = $prog => {
+import 'colors';
+import * as inquirer from 'inquirer';
+import { snakeCase } from 'lodash';
+import * as shell from 'shelljs';
+import * as licenses from 'spdx-license-list';
+import * as fs from 'fs';
+import * as path from 'path';
+
+module.exports = ($prog, settings) => {
   $prog
-    .command('create', 'Create a new Application')
+    .command('brew', 'Brew a new application')
     .argument('<app_name>', 'Application Name')
-    .option('--flavor <flavor>', 'Which <flavor> do you like (ts-node, webpack)')
-    .action((args, options, logger) => {
-      console.log({
-        args: args,
-        options: options
-      });
+    .option('--flavor <flavor>', 'Which <flavor> do you like (ts-node, webpack)', ['ts-node', 'webpack'])
+    .action(async (args, options, logger) => {
+      console.log('');
+      console.log('Welcome to Tea, please give us the instruction to brew you a nice application.'.white.bold);
+      console.log('');
+
+      const answers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'appName',
+          message: 'What is the application name',
+          default: args.appName,
+          transformer: snakeCase
+        },
+        {
+          type: 'input',
+          name: 'license',
+          message: 'What is License do you prefer',
+          default: 'MIT',
+          validate: opt => !!licenses[opt]
+        },
+        {
+          type: 'list',
+          when: !options.flavor,
+          name: 'flavor',
+          message: 'Please Select the flavor that you like it:',
+          choices: ['ts-node', 'webpack']
+        }
+      ]);
+
+      const $target = path.resolve(shell.pwd().toString(), answers.appName);
+      const $local = settings.root;
+      const $templatePath = `${$local}/templates/${options.flavor || answers.flavor}/`;
+
+      if (fs.existsSync($templatePath)) {
+        shell.mkdir('-p', $target);
+        logger.info('Copying files…');
+        shell.cp('-R', `${$templatePath}/*`, $target);
+        logger.info(' The files have been copied!');
+      } else {
+        logger.error(`The requested template for ${args.appName} wasn't found.`);
+        process.exit(1);
+      }
+      console.log({ args, options, settings, $target, $local, answers });
     });
 };
